93a6d6e4b978d694033ba6041393104364fcc954
diez.manuel98@gmail.com <diez.manuel98@gmail.com>
2021-12-24 13:40:29 -0300

    funcion restar

--- maths.js
+++ maths.js
@@ -1,13 +1,18 @@
 function sumarDosNumeros(n1, n2){
     return n1 + n2
 }
+function restarDosNumeros(n1, n2){
+    return n1 - n2;
+}
+
 
 function twoTimesNumber(n){
     return 2*n;
 }
 
 console.log("__________")
-console.log(twoTimesNumber(5))
-console.log("__________")
 console.log(sumarDosNumeros(7, 12))
 console.log("__________")
+console.log(twoTimesNumber(5))
+console.log("____________")
+console.log(restarDosNumeros(5, 3))
