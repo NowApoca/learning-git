3ae68f3e307f65449b4183ed746a806fcadb10de
diez.manuel98@gmail.com <diez.manuel98@gmail.com>
2021-12-24 13:37:29 -0300

    funcion sumar

--- maths.js
+++ maths.js
@@ -1,7 +1,13 @@
-
+function sumarDosNumeros(n1, n2){
+    return n1 + n2
+}
 
 function twoTimesNumber(n){
     return 2*n;
 }
 
+console.log("__________")
 console.log(twoTimesNumber(5))
+console.log("__________")
+console.log(sumarDosNumeros(7, 12))
+console.log("__________")
